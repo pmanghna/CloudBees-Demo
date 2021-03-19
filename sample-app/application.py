561ba239288004b0054b3fe8895c985b1b79a4a9
pmanghna <37938426+pmanghna@users.noreply.github.com>
2021-03-19 14:26:15 -0700

    Create application.py

---
+++ sample-app/application.py
@@ -0,0 +1,150 @@
+import logging
+import logging.handlers
+
+from wsgiref.simple_server import make_server, WSGIServer
+from SocketServer import ThreadingMixIn
+
+# Create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Handler 
+LOG_FILE = '/tmp/sample-app.log'
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
+handler.setLevel(logging.INFO)
+
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Add Formatter to Handler
+handler.setFormatter(formatter)
+
+# add Handler to Logger
+logger.addHandler(handler)
+
+welcome = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <!--
+    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.Amazon/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+  -->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Welcome</title>
+  <style>
+  body {
+    color: #ffffff;
+    background-color: #E0E0E0;
+    font-family: Arial, sans-serif;
+    font-size:12px;
+    -moz-transition-property: text-shadow;
+    -moz-transition-duration: 4s;
+    -webkit-transition-property: text-shadow;
+    -webkit-transition-duration: 4s;
+    text-shadow: none;
+  }
+  body.blurry {
+    -moz-transition-property: text-shadow;
+    -moz-transition-duration: 4s;
+    -webkit-transition-property: text-shadow;
+    -webkit-transition-duration: 4s;
+    text-shadow: #fff 0px 0px 25px;
+  }
+  a {
+    color: #0188cc;
+  }
+  .textColumn, .linksColumn {
+    padding: 2em;
+  }
+  .textColumn {
+    position: absolute;
+    top: 0px;
+    right: 0%;
+    bottom: 0px;
+    left: 0px;
+
+    text-align: center;
+    padding-top: 11em;
+    background-color: #24D0EB;
+  }
+  .textColumn p {
+    width: 75%;
+    float:right;
+  }
+  .linksColumn {
+    position: relative;
+    top:0px;
+    right: 0px;
+    bottom: 0px;
+    left: 100%;
+    background-color: #A9A9A9;
+  }
+
+  h1 {
+    font-size: 500%;
+    font-weight: normal;
+    margin-bottom: 0em;
+  }
+  h2 {
+    font-size: 200%;
+    font-weight: normal;
+    margin-bottom: 0em;
+  }
+  ul {
+    padding-left: 1em;
+    margin: 0px;
+  }
+  li {
+    margin: 1em 0em;
+  }
+  </style>
+</head>
+<body id="sample">
+  <div class="textColumn">
+    <h1>Hello!</h1>
+    <h2>Your application is now deployed in AWS ECS in your own dedicated environment in AWS Cloud</h2>
+    <h2>This application is deployed using CloudBees</h2>
+  </div>
+
+</body>
+</html>
+"""
+
+def application(environ, start_response):
+    path    = environ['PATH_INFO']
+    method  = environ['REQUEST_METHOD']
+    if method == 'POST':
+        try:
+            if path == '/':
+                request_body_size = int(environ['CONTENT_LENGTH'])
+                request_body = environ['wsgi.input'].read(request_body_size)
+                logger.info("Received message: %s" % request_body)
+            elif path == '/scheduled':
+                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
+        except (TypeError, ValueError):
+            logger.warning('Error retrieving request body for async work.')
+        response = ''
+    else:
+        response = welcome
+    status = '200 OK'
+    headers = [('Content-type', 'text/html')]
+
+    start_response(status, headers)
+    return [response]
+
+class ThreadingWSGIServer(ThreadingMixIn, WSGIServer): 
+    pass
+
+if __name__ == '__main__':
+    httpd = make_server('', 8000, application, ThreadingWSGIServer)
+    print "Serving on port 8000..."
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
